75df2693e16b6aeebbc2f3273cbf34e2805cae3b
gabrielebottari <gabriele.bottari1@gmail.com>
2023-11-30 13:56:54 +0100

    END exercise

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+let nome = prompt("inserisci il tuo nome", "nome");
+let cognome = prompt("inserisci il tuo cognome", "cognome");
+let colorePreferito = prompt("inserisci il tuo colore preferito", "colore");
+
+document.getElementById("password").innerHTML = nome + cognome + colorePreferito + "114";
