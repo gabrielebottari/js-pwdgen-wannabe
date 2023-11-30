496389951ec1e0cd8566b5acc426fac3ba5afa5f
gabrielebottari <gabriele.bottari1@gmail.com>
2023-11-30 14:01:37 +0100

    ADD console.log

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,10 @@
 let nome = prompt("inserisci il tuo nome", "nome");
+console.log(nome);
+
 let cognome = prompt("inserisci il tuo cognome", "cognome");
+console.log(cognome);
+
 let colorePreferito = prompt("inserisci il tuo colore preferito", "colore");
+console.log(colorePreferito);
 
 document.getElementById("password").innerHTML = nome + cognome + colorePreferito + "114";
